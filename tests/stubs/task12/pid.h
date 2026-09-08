b580f399316901e74acc5317aab25c148c9bc009
单鑫城 <2653066422@qq.com>
2026-09-08 21:40:58 +0800

    test: guard host PID profile stub

--- tests/stubs/task12/pid.h
+++ tests/stubs/task12/pid.h
@@ -1,6 +1,8 @@
 #ifndef TASK12_PID_H
 #define TASK12_PID_H
 typedef struct { float output; } PID;
+#ifndef HOST_PID_PROFILE_T_DEFINED
+#define HOST_PID_PROFILE_T_DEFINED
 typedef struct {
     float kp;
     float ki;
@@ -8,6 +10,7 @@ typedef struct {
     float max_integral;
     float max_output;
 } PID_Profile_t;
+#endif
 extern PID mypid;
 extern PID_Profile_t PID_Profiles[5];
 extern float targetValue, feedbackValue;
